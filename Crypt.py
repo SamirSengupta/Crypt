d115f2565a185d4ab70fe561ef02952ad87b292d
Samir Sengupta <51231316+SamirSengupta@users.noreply.github.com>
2023-05-04 23:59:39 +0530

    Update Crypt.py

--- Crypt.py
+++ Crypt.py
@@ -1,18 +1,23 @@
-import requests
-import pandas as pd
-import streamlit as st
-
 def get_cryptocurrency_recommendation(investment_amount):
     url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,litecoin,ripple&vs_currencies=inr"
     response = requests.get(url)
+    if response.status_code != 200:
+        st.error("Failed to retrieve data from API.")
+        return None, None, None
     data = response.json()
     df = pd.DataFrame(data)
+    if 'inr' not in df.columns:
+        st.error("Data does not contain 'inr' column.")
+        return None, None, None
     df = df.transpose()
     df['value'] = investment_amount / df['inr']
     df = df.sort_values('value', ascending=False)
     recommendation = df.iloc[0].name
     change_url = f"https://api.coingecko.com/api/v3/coins/{recommendation}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false"
     change_response = requests.get(change_url)
+    if change_response.status_code != 200:
+        st.error("Failed to retrieve data from API.")
+        return None, None, None
     change_data = change_response.json()
     change_percent = change_data['market_data']['price_change_percentage_24h']
     if change_percent > 0:
@@ -26,17 +31,3 @@ def get_cryptocurrency_recommendation(investment_amount):
     df['change_data'] = df['change_response'].map(lambda x: x.json())
     df['change_percent'] = df['change_data'].map(lambda x: x['market_data']['price_change_percentage_24h'])
     return recommendation, reason, df
-
-st.title("Cryptocurrency Recommendation Tool")
-
-investment_amount = st.number_input("Enter the amount you want to invest in INR:", value=1000, step=1000)
-if investment_amount > 0:
-    recommendation, reason, df = get_cryptocurrency_recommendation(investment_amount)
-    st.write(f"Based on your investment amount of {investment_amount} INR, we recommend buying {recommendation}. {reason}")
-    st.write("Here are the top 3 cryptocurrencies you may consider:")
-    top_3 = df.iloc[1:4][['value', 'change_percent']]
-    st.table(top_3)
-    st.write("Here are the current values of all the cryptocurrencies:")
-    st.table(df[['inr', 'change_percent']])
-else:
-    st.warning("Please enter a valid investment amount.")
